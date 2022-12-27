e4b2268f2952a717f9919326e39e3be6ce21ba25
fqds <121273hvv@gmail.com>
2022-12-27 07:35:29 +0300

    postgres connection + some tests

--- internal/app/apiserver/config.go
+++ internal/app/apiserver/config.go
@@ -1,15 +1,19 @@
 package apiserver
 
+import "github.com/fqds/rest-api-example/internal/app/store"
+
 // Config ...
 type Config struct {
 	BindAddr string `toml:"bind_addr"`
 	LogLevel string `toml:"log_level"`
+	Store    *store.Config
 }
 
 // NewConfig ...
-func NewConfig() * Config {
-	return &Config {
+func NewConfig() *Config {
+	return &Config{
 		BindAddr: ":8080",
 		LogLevel: "debug",
+		Store:    store.NewConfig(),
 	}
 }
